e9215b8a8aa55730058aaf87fa93871cb0319ca1
Keeper191 <keepentertainingyourself@gmail.com>
2024-07-28 23:33:56 +0530

    finding fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+set -e
 
 # Initialize the database
-python -c "from app import create_app, db; app, _ = create_app(); app.app_context().push(); db.create_all()"
+python -c "from app import create_app, db; app, _ = create_app(); with app.app_context(): db.create_all()"
 
 # Run the Flask application
-exec python app.py
+exec python /app/app.py
