d7bc7edc62de51844228b6803610ed48f925b293
Keeper191 <keepentertainingyourself@gmail.com>
2024-07-29 22:15:05 +0530

    entrypoint fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Initialize the database
-python -c "from app import create_app, db; app, _ = create_app(); with app.app_context(): db.create_all()"
+python -c "from app import create_app, db; app, _ = create_app(); app.app_context().push(); db.create_all()"
 
 # Run the Flask application
 exec python /app/app.py
